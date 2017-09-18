05553fa3d0f2e9f5a40ee5fe304b203e48cebd9a
Charlie-Schlager <31775196+Charlie-Schlager@users.noreply.github.com>
2017-09-18 10:42:59 -0400

    Update Shift_Register.ino
    Adding IR

--- Classwork/September 19/Shift_Register.ino
+++ Classwork/September 19/Shift_Register.ino
@@ -1,11 +1,23 @@
+#include <IRremote.h>
+
+int RECV_PIN = 11;  //IR Receiving Pin
+IRrecv irrecv(RECV_PIN); 
+decode_results results;
+
 int latchPin = 5;
 int clockPin = 6;
 int dataPin = 4;
 
+
 byte leds = 0;
+
 
 void setup() 
 {
+   Serial.begin(9600); //setting up the serial monitor so I can get the HEX codes of each button and map them
+  
+   irrecv.enableIRIn(); //Enabling the IR Reciever
+  
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);  
   pinMode(clockPin, OUTPUT);
@@ -13,6 +25,12 @@ void setup()
 
 void loop() 
 {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+  delay(100);
+}
   leds = 0;
   updateShiftRegister();
   delay(500);
