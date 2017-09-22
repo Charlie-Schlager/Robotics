7decfb8f660928819087e15ae8fb953b99b61fd4
Charlie-Schlager <cschlager19@choate.edu>
2017-09-22 11:22:18 -0400

    Added Video

--- Classwork/September 20/LCD_Distance_Visualizer/LCD_Distance_Visualizer.ino
+++ Classwork/September 20/LCD_Distance_Visualizer/LCD_Distance_Visualizer.ino
@@ -1,9 +1,15 @@
+#include <NewPing.h>
+
+
 #include <LiquidCrystal.h> // includes the LiquidCrystal Library
+
 LiquidCrystal lcd(0, 1, 8, 9, 10, 11); // Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7)
 const int trigPin = 12;
 const int echoPin = 13;
 long duration;
 int distanceCm, distanceInch;
+
+
 void setup() {
 lcd.begin(16,2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
 pinMode(trigPin, OUTPUT);
