4cfb9da2de8cf62ae9ad77c1d9903cb9a9205a96
Charlie-Schlager <cschlager19@choate.edu>
2017-09-25 14:41:23 -0400

    Added Video

--- Classwork/September 25/Servo_Knob_LED_Levels/Servo_Knob_LED_Levels.ino
+++ Classwork/September 25/Servo_Knob_LED_Levels/Servo_Knob_LED_Levels.ino
@@ -6,8 +6,16 @@ Servo servo;
  
 void setup() 
 { 
+  pinMode(7, OUTPUT);
+   pinMode(6, OUTPUT);
+    pinMode(5, OUTPUT);
+     pinMode(4, OUTPUT);
+      pinMode(3, OUTPUT);
+       pinMode(2, OUTPUT);
   servo.attach(servoPin);  
+  Serial.begin(9600);
 } 
+
  
 void loop() 
 { 
@@ -21,46 +29,52 @@ void loop()
   digitalWrite(5, LOW);
   digitalWrite(4, LOW);
   digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
   }
 
-  if (201 < analogRead(potPin) <= 400) {
+  if (201 < analogRead(potPin) && analogRead(potPin) <= 400) {
   digitalWrite(7, HIGH);
   digitalWrite(6, LOW);
   digitalWrite(5, LOW);
   digitalWrite(4, LOW);
   digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
   }
 
-  if (401 < analogRead(potPin) <= 600) {
+  if (401 < analogRead(potPin) && analogRead(potPin) <= 600) {
   digitalWrite(7, HIGH);
   digitalWrite(6, HIGH);
   digitalWrite(5, LOW);
   digitalWrite(4, LOW);
   digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
   }
 
-  if (601 < analogRead(potPin) <= 800) {
+  if (601 < analogRead(potPin) && analogRead(potPin) <= 800) {
   digitalWrite(7, HIGH);
   digitalWrite(6, HIGH);
   digitalWrite(5, HIGH);
   digitalWrite(4, LOW);
   digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
   }
   
-  if (801 < analogRead(potPin) <= 1000) {
+  if (801 < analogRead(potPin) && analogRead(potPin) <= 1000) {
   digitalWrite(7, HIGH);
   digitalWrite(6, HIGH);
   digitalWrite(5, HIGH);
   digitalWrite(4, HIGH);
   digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
     }
 
-  if (1001 < analogRead(potPin) <= 1023) {
+  if (1001 < analogRead(potPin) && analogRead(potPin) <= 1024) {
   digitalWrite(7, HIGH);
   digitalWrite(6, HIGH);
   digitalWrite(5, HIGH);
   digitalWrite(4, HIGH);
   digitalWrite(3, HIGH);
+  digitalWrite(2, HIGH);
     }
-
+Serial.println(analogRead(potPin));
 } 
